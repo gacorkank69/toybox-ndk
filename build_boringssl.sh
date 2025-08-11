05f3b4b51d75f96f96c5a827f430de19d443714f
bintang774 <108184157+bintang774@users.noreply.github.com>
2025-08-11 13:41:48 +0800

    Pass `-DOPENSSL_SMALL=1` to the cmake
    Signed-off-by: bintang774 <108184157+bintang774@users.noreply.github.com>

--- build_boringssl.sh
+++ build_boringssl.sh
@@ -3,6 +3,7 @@ cd boringssl
 cmake -DANDROID_ABI=arm64-v8a \
     -DANDROID_PLATFORM=android-35 \
     -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_PATH}/build/cmake/android.toolchain.cmake \
+    -DOPENSSL_SMALL=1 \
     -GNinja -B build
 cmake --build build
 cd ..
